1f62c68502ae2a52c5767c7a4653c477d6227f3f
Ari Stassinopoulos <stassinopoulosari@gmail.com>
2025-04-09 18:11:58 -0700

    Update scraper

--- scraper/index.js
+++ scraper/index.js
@@ -1,11 +1,17 @@
-const http = require("http"),
+const http = require("https"),
 	gtfs = require("./res/gtfs-realtime.js"),
 	toadScheduler = require("toad-scheduler"),
 	secrets = require("./secrets.js"),
 	pg = require("pg"),
 	appID = secrets.appID,
-	vehiclePositionURL = `http://developer.trimet.org/ws/V1/VehiclePositions/?appID=${appID}`,
-	tripUpdateURL = `http://developer.trimet.org/ws/V1/TripUpdate/?appID=${appID}`;
+	openWeatherKey = secrets.openWeatherKey,
+	location = {
+		lat: 45.519135,
+		lon: -122.6795,
+	},
+	vehiclePositionURL = `https://developer.trimet.org/ws/V1/VehiclePositions/?appID=${appID}`,
+	tripUpdateURL = `https://developer.trimet.org/ws/V1/TripUpdate/?appID=${appID}`,
+	weatherURL = `https://api.openweathermap.org/data/3.0/onecall?lat=${location.lat}&lon=${location.lon}&exclude=hourly,minutely,daily&appid=${openWeatherKey}`;
 
 // Connect to Database
 const pool = new pg.Pool({
@@ -18,15 +24,28 @@ const attemptConnection = async () => (client = await pool.connect()),
 		try {
 			await client.query(`
 				CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE trimet_observation(
+				# DROP TABLE IF EXISTS trimet_observation CASCADE;
+				# DROP TABLE IF EXISTS route_description CASCADE;
+				# DROP TABLE IF EXISTS trip_update CASCADE;
+				# DROP TABLE IF EXISTS weather_description CASCADE;
+				# DROP TABLE IF EXISTS weather_observation CASCADE;
+
+				CREATE TABLE trimet_observation(
 					trimet_observation_id
 						UUID
 						PRIMARY KEY
 						NOT NULL
 						DEFAULT GEN_RANDOM_UUID(),
 					date TIMESTAMPTZ NOT NULL DEFAULT NOW()
-);
-CREATE TABLE trip_update(
+				);
+				CREATE TABLE route_description(
+					route_id
+						NUMERIC(4, 0)
+						NOT NULL
+						PRIMARY KEY,
+					route_label TEXT
+				);
+				CREATE TABLE trip_update(
 					trip_update_id
 						UUID
 						PRIMARY KEY
@@ -38,17 +57,43 @@ CREATE TABLE trip_update(
 						REFERENCES trimet_observation(trimet_observation_id),
 					vehicle_id NUMERIC(5, 0),
 					stop_in_sequence NUMERIC(3, 0),
+					route_id
+						NUMERIC(4, 0)
+						NOT NULL
+						REFERENCES route_description(route_id),
 					trip_id NUMERIC(10, 0) NOT NULL,
-					route_id NUMERIC(4, 0) NOT NULL,
-					route_label TEXT,
-					status_data_exists BOOLEAN,
 					status_departure_delay_seconds NUMERIC(10, 0),
 					status_arrival_delay_seconds NUMERIC(10,0)
-);
+				);
+				CREATE TABLE weather_description(
+					weather_description_id NUMERIC(3,0) NOT NULL PRIMARY KEY,
+					text_description TEXT NOT NULL
+				);
+				CREATE TABLE weather_observation(
+					weather_observation_id
+						UUID
+						PRIMARY KEY
+						NOT NULL
+						DEFAULT GEN_RANDOM_UUID(),
+					date TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+					temperature_kelvin NUMERIC(6, 3),
+					feels_like_kelvin NUMERIC(6, 3),
+					visibility NUMERIC(6, 1),
+					wind_speed_m_s NUMERIC(6, 2),
+					wind_gust_m_s NUMERIC(6, 2),
+					weather_description_id NUMERIC(3,0)
+						NOT NULL
+						REFERENCES weather_description(weather_description_id),
+					next_hour_rain_mm NUMERIC(5, 2)
+				);
 			`);
 			console.log("Set up table");
 		} catch (err) {
-			console.log("Table is already set up");
+			console.log(
+				"Table is already set up. Attempting initial observations",
+			);
+			observeWeather();
+			observeTrimet();
 		}
 	},
 	connectInterval = setInterval(
@@ -65,7 +110,7 @@ CREATE TABLE trip_update(
 		2000,
 	);
 
-const parseFeedMessageFromURI = (feedURL) =>
+const downloadVehicleFeed = (feedURL) =>
 		new Promise((resolve) =>
 			http.get(feedURL, (feedResponse) => {
 				const data = [];
@@ -83,8 +128,8 @@ const parseFeedMessageFromURI = (feedURL) =>
 	scrapeVehicles = () =>
 		new Promise((resolve) =>
 			Promise.all([
-				parseFeedMessageFromURI(vehiclePositionURL),
-				parseFeedMessageFromURI(tripUpdateURL),
+				downloadVehicleFeed(vehiclePositionURL),
+				downloadVehicleFeed(tripUpdateURL),
 			]).then((results) => {
 				const [vehiclePositions, tripUpdate] = results,
 					vehicleStatuses = {};
@@ -138,42 +183,107 @@ const parseFeedMessageFromURI = (feedURL) =>
 				return resolve(vehicleStatuses);
 			}),
 		),
+	downloadWeather = (feedURL) =>
+		new Promise((resolve, reject) => {
+			http.get(feedURL, (feedResponse) => {
+				const data = [];
+				feedResponse.on("data", (chunk) => data.push(chunk));
+				feedResponse.on("end", () => {
+					const mergedData = Buffer.concat(data),
+						message = JSON.parse(mergedData.toString("utf8"));
+					return resolve(message);
+				});
+			});
+		}),
+	scrapeWeather = async () => {
+		const weatherData = await downloadWeather(weatherURL),
+			currentWeather = weatherData.current;
+		return {
+			temperatureKelvin: currentWeather.temp,
+			feelsLikeKelvin: currentWeather.feels_like,
+			visibility: currentWeather.visibility,
+			windSpeed: currentWeather.wind_speed,
+			windGust: currentWeather.wind_gust,
+			weatherCode: currentWeather.weather[0].id,
+			weatherDescription: currentWeather.weather[0].description,
+			rain: (currentWeather.rain ?? { "1h": 0 })["1h"],
+		};
+	},
+	saveWeather = async (weather) => {
+		await client.query(
+			`
+			INSERT INTO weather_description(
+				weather_description_id,
+				text_description
+			) VALUES($1, $2) ON CONFLICT(weather_description_id) DO NOTHING;
+			`,
+			[weather.weatherCode, weather.weatherDescription],
+		);
+		await client.query(
+			`
+			INSERT INTO weather_observation(
+				temperature_kelvin,
+				feels_like_kelvin,
+				visibility,
+				wind_speed_m_s,
+				wind_gust_m_s,
+				weather_description_id,
+				next_hour_rain_mm
+			) VALUES($1, $2, $3, $4, $5, $6, $7);
+			`,
+			[
+				weather.temperatureKelvin,
+				weather.feelsLikeKelvin,
+				weather.visibility,
+				weather.windSpeed,
+				weather.windGust,
+				weather.weatherCode,
+				weather.rain,
+			],
+		);
+	},
 	saveTrimetObservation = async (observationID, vehicleStatuses) => {
 		const entryPromises = Object.entries(vehicleStatuses).map(
 			(entry) => async () => {
-				const [key, value] = entry,
-					query = await client.query(
-						`
-INSERT INTO vehicle_statuses(
-trimet_observation_id,
-vehicle_id,
-stop_in_sequence,
-trip_id,
-route_id,
-route_label,
-status_data_exists,
-status_departure_delay_seconds,
-status_arrival_delay_seconds
-) VALUES($1, $2, $3, $4, $5, $6, $7);
-`,
-						[
-							observationID,
-							parseInt(key),
-							value.stopInSequence,
-							value.tripID,
-							value.routeID,
-							value.routeLabel,
-							value.status.dataExists,
-							value.status.departureDelaySeconds,
-							value.status.arrivalDelaySeconds,
-						],
-					);
+				const [key, value] = entry;
+				await client.query(
+					`
+					INSERT INTO route_description(
+						route_id,
+						route_label
+					) VALUES($1, $2)
+						ON CONFLICT (route_id)
+						DO NOTHING;
+					`,
+					[value.routeID, value.routeLabel],
+				);
+				await client.query(
+					`
+					INSERT INTO vehicle_statuses(
+						trimet_observation_id,
+						vehicle_id,
+						stop_in_sequence,
+						trip_id,
+						route_id,
+						status_departure_delay_seconds,
+						status_arrival_delay_seconds
+					) VALUES($1, $2, $3, $4, $5, $6, $7);
+					`,
+					[
+						observationID,
+						parseInt(key),
+						value.stopInSequence,
+						value.tripID,
+						value.routeID,
+						value.status.departureDelaySeconds,
+						value.status.arrivalDelaySeconds,
+					],
+				);
 			},
 		);
 		return await Promise.all(entryPromises);
 	},
 	observeTrimet = () => {
-		const date = new Date();
 		scrapeVehicles().then(async (vehicleStatuses) => {
 			// Add new observation
 			let observationID;
@@ -186,17 +296,34 @@ INSERT INTO trimet_observation DEFAULT VALUES RETURNING *;
 				}
 				observationID = observationQuery.rows[0].trimet_observation_id;
 				await saveTrimetObservation(observationID, vehicleStatuses);
-				console.log("Successfully saved obserxvations");
+				console.log("Successfully saved trimet data");
 			} catch (error) {
 				console.error(error);
 			}
 		});
+	},
+	observeWeather = async () => {
+		try {
+			await scrapeWeather().then((weather) => saveWeather(weather));
+			console.log("Successfully saved weather data");
+		} catch (error) {
+			console.error(error);
+		}
 	},
 	scheduler = new toadScheduler.ToadScheduler(),
 	observeTrimetTask = new toadScheduler.Task("observeTrimet", observeTrimet),
 	observeTrimetJob = new toadScheduler.SimpleIntervalJob(
 		{ minutes: 1 },
 		observeTrimetTask,
+	),
+	observeWeatherTask = new toadScheduler.Task(
+		"observeWeather",
+		observeWeather,
+	),
+	observeWeatherJob = new toadScheduler.SimpleIntervalJob(
+		{ minutes: 1 },
+		observeWeatherTask,
 	);
 
 scheduler.addSimpleIntervalJob(observeTrimetJob);
+scheduler.addSimpleIntervalJob(observeWeatherJob);
